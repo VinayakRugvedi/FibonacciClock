39779ae6164b4cf4f3fee1c401a565a3c4f942c4
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-01 16:23:19 +0530

    Added initial point of possibilities

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const possibilities = {
+  1 : ["one"],
+  2 : [["one", "one"], ["two"]],
+  3 : [["one", "two"], ["three"]],
+  4 : [["one", "one", "two"], ["one", "three"]],
+  5 : [["one", "one", "three"], ["two", "three"], ["five"]],
+  6 : [["one", "five"], ["one", "two", "three"]],
+  7 : [["one", "one", "five"], ["one", "one", "two", "three"], ["two", "five"]],
+  8 : [["one", "two", "five"], ["three", "five"]],
+  9 : [["one", "three", "five"], ["one", "one", "two", "five"]],
+  10 : [["one", "one", "three", "five"], ["two", "three", "five"]],
+  11 : ["one", "two", "three", "five"],
+  12 : ["one", "one", "two", "three", "five"]
+}
